fa8c193c42bb65f09ff267af753135a950305fd3
milad_ahmadi <milad.ahmadi.job@gmail.com>
2022-11-13 12:02:58 +0330

    add controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodcourt/utils/ScrollClass.dart';
+import 'package:foodcourt/utils/util.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      scrollBehavior: AppScrollBehavior(),
+      title: 'FoodCourt',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,6 +24,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -28,14 +32,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _currentPage=0;
+  PageController _pageController=PageController(
+    viewportFraction: 0.3
+  );
+
+  void _foodControllerListener(){
+      setState(() {
+        _currentPage=_pageController.page!;
+      });
+  }
+
+  @override
+  void initState() {
+    _pageController.addListener(_foodControllerListener);
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _pageController.removeListener(_foodControllerListener);
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: Container(),
+      body: Stack(
+        children: [
+          Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              height: 100,
+              child: Container(
+                color: Colors.red,
+              )),
+          PageView.builder(
+            controller: _pageController,
+            itemCount: foods.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+            final food=foods[index];
+            return Image.asset(food.imagePath);
+          },)
+        ],
+      ),
     );
   }
 }
